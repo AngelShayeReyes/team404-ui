36e4d60178306dbc91379817ebb1424ae27e7966
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-08-23 15:31:12 +0100

    api call example

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const nunjucks = require('nunjucks')
+const axios = require('axios');
 
 let port = 7999;
 
@@ -11,9 +12,23 @@ nunjucks.configure('app/views', {
 app.set('view engine', 'njk');
 
 app.get('/mvp', async (req, res) => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(function (response) {
+        // handle success
+            console.log(response.data.date);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
     res.render('mvp')
 });
 
 app.listen(port, function() {
     console.log('Express started')
  });
+
+ 
