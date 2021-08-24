2c7763b490c22431d21e1630d2dd6111a41db045
Angel Reyes <angelshaye.reyes@kainos.com>
2021-08-24 17:17:22 +0100

    Added ViewJobRolesTest.js

---
+++ app/test/ViewJobRolesTest.js
@@ -0,0 +1,37 @@
+require('chromedriver');
+const assert = require('assert');
+const { json } = require('express');
+const {Builder, Key, By, until} = require('selenium-webdriver');
+
+describe('Checkout View Job Roles', function () {
+    let driver;
+
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('http://localhost:7999/viewjobroles');
+    });
+
+    it('Check webpage is up, running and showing the correct view', async function() {
+        let page_header = await driver.findElement(By.id("page_title")).getText(); 	
+        console.log(JSON.stringify(page_header));
+        assert.equal(page_header, 'View Job Roles');              
+    });
+
+    it('Check Title from API is being displayed and entered correctly within the table', async function(){ 
+        let innerText = await driver.findElement(By.xpath("//table/tbody/tr[2]/td[1]")).getText(); 	
+        assert.equal(innerText, 'Head of People Operations');  
+ 
+    });
+
+    it('Check Contract Type from API is being displayed and entered correctly within the table', async function(){ 
+        let innerText = await driver.findElement(By.xpath("//table/tbody/tr[1]/td[2]")).getText(); 	
+        assert.equal(innerText, 'Full time');  
+ 
+    });
+
+
+    after(() => driver && driver.quit());
+
+    
+})
+
