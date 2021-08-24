d4c4b2a2f785b3cb009405eeed28f1fedbfbbaaf
Angel Reyes <angelshaye.reyes@kainos.com>
2021-08-24 16:07:10 +0100

    Added ViewJobRoles.njk

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const nunjucks = require('nunjucks')
 const axios = require('axios');
+const dateFilter = require('nunjucks-date-filter');
 
 let port = 7999;
 
-nunjucks.configure('app/views', {
+let env = nunjucks.configure('app/views', {
     express: app
 });
-
+env.addFilter('date', dateFilter);
 app.use(express.static('app'));
 
 app.set('view engine', 'njk');
@@ -18,6 +19,21 @@ app.get('/mvp', async (req, res) => {
         .then(function (response) {
         // handle success
         res.render('mvp', { job_roles: response.data} ) 
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
+});
+
+app.get('/viewjobroles', async (req, res) => {
+    axios.get('http://localhost:8080/job-roles')
+        .then(function (response) {
+        // handle success
+        res.render('ViewJobRoles', { job_roles: response.data} ) 
     })
     .catch(function (error) {
         // handle error
