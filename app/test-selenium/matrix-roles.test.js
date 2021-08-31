1f4e8cf07bc204bdf9f835f8f23f804885e438a0
Angel Reyes <angelshaye.reyes@kainos.com>
2021-08-31 15:28:53 +0100

    Added passing selenium tests and added structure for unit tests

---
+++ app/test-selenium/matrix-roles.test.js
@@ -0,0 +1,44 @@
+require('chromedriver');
+const assert = require('assert');
+const { json } = require('express');
+const {Builder, Key, By, until} = require('selenium-webdriver');
+const script = require('jest');
+ 
+const url = 'http://localhost:7999/viewmatrixofroles'
+
+describe('Checkout View Matrix of Roles', function () {
+    let driver;
+
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get(url);
+    });
+
+    it('Check webpage is up, running and showing the correct view', async function() {
+        let page_header = await driver.findElement(By.id("page_title")).getText(); 	
+        console.log(JSON.stringify(page_header));
+        assert.equal(page_header, 'Engineering Capability');              
+    });
+
+    it('Check Job Family from API is being displayed and entered correctly within the table', async function(){ 
+        let innerText = await driver.findElement(By.xpath("//table/thead/tr[1]/th[1]")).getText(); 	
+        assert.equal(innerText, 'Engineering Strategy and Planning');  
+ 
+    });
+
+    it('Check Band Level from API is being displayed and entered correctly within the table', async function(){ 
+        let innerText = await driver.findElement(By.xpath("//table/tbody/tr[1]/th[1]")).getText(); 	
+        assert.equal(innerText, 'Leadership Community');  
+ 
+    });
+
+    it('Check Data from API is being displayed and entered correctly within the table', async function(){ 
+        let innerText = await driver.findElement(By.xpath("//table/tbody/tr[2]/td[3]")).getText(); 	
+        assert.equal(innerText, 'Principal Architect');  
+ 
+    });
+
+    after(() => driver && driver.quit());
+    
+})
+
