6694859f651cbb9f4d55a23bdfb27656fb4759a1
Angel Reyes <angelshaye.reyes@kainos.com>
2021-08-31 11:10:18 +0100

    Added pictures to navigation

---
+++ app/test-selenium/navigation.test.js
@@ -0,0 +1,47 @@
+require('chromedriver');
+const assert = require('assert');
+const { json } = require('express');
+const {Builder, Key, By, until} = require('selenium-webdriver');
+const script = require('jest');
+ 
+const url = 'http://localhost:7999/navigation'
+
+describe('Checkout Navigation Page', function () {
+    let driver;
+
+    before(async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get(url);
+    });
+
+    it('Check webpage is up, running and showing the correct view', async function() {
+        let page_header = await driver.findElement(By.id("page_title")).getText(); 	
+        assert.equal(page_header, 'Kainos Job Application');              
+    });
+
+    it('Check link from navigation to job roles', async function() {
+        await driver.findElement(By.linkText("Job Roles")).click()
+        let page_header = await driver.findElement(By.id("page_title")).getText(); 	
+        assert.equal(page_header, 'View Job Roles');           
+    });
+
+    it('Check link from navigation to competencies for engineering capability', async function() {
+        await driver.navigate().back();
+        await driver.findElement(By.linkText("Competencies for the Engineering Capability")).click()
+        let page_header = await driver.findElement(By.id("page_title")).getText(); 	
+        assert.equal(page_header, 'Kainos Competencies for the Engineering Capability');               
+    });
+
+
+    it('Check link from navigation to matrix of roles', async function() {
+        await driver.navigate().back();
+        await driver.findElement(By.linkText("Matrix of Roles")).click()
+        let page_header = await driver.findElement(By.id("page_title")).getText(); 	
+        assert.equal(page_header, 'Engineering Capability');            
+    });
+
+
+    after(() => driver && driver.quit());
+    
+})
+
