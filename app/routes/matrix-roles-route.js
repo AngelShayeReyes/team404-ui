ce03515176c7598a6c4704b5d048fd7df5e68b6f
AngelShayeReyes <88448347+AngelShayeReyes@users.noreply.github.com>
2021-09-03 09:58:51 +0100

    Removed matrixRolesService from matrix-roles-route.js

--- app/routes/matrix-roles-route.js
+++ app/routes/matrix-roles-route.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const matrixRolesService = require('../services/matrix-roles-service');
 
 router.get("/", async (req, res) => {
     res.render('view-matrix-roles');
