054e4eddd25e1a3185571af1451e32d663cb153b
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-09-06 14:41:17 +0100

    use api endpoint /bands to populate bands dropdown

--- app/routes/add-new-role-route.js
+++ app/routes/add-new-role-route.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const competenciesService = require('../services/add-new-role-service');
 
 router.get("/", async (req, res) => {
-    res.render('add-new-role');
+    res.render('add-new-role', {bands: await competenciesService.getAllBands()});
 });
 
 router.post("/", async (req, res) => {
     res.locals.errormessage = "Not implemented yet!";
-    res.render('add-new-role');
+    res.render('add-new-role', req.body);
 });
 
 module.exports = router
--- app/services/add-new-role-service.js
+++ app/services/add-new-role-service.js
@@ -3,3 +3,16 @@ const axios = require('axios');
 exports.addNewRole = async (newRole) => { 
     
 }
+
+exports.getAllBands = async () => {
+    let results;
+    await axios.get('http://localhost:8080/bands')
+        .then(response => {
+            results = response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            //res.render('error-page', { error_code: error.response.data} )
+        });
+    return results;
+}
