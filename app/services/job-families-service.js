610a38f3faa502944f8084d2c24b4aa96960ce62
Priyanka Jhooty <priyanka.jhooty@kainos.com>
2021-09-07 18:27:55 +0100

    job families page, one selenium test still failing

--- app/services/job-families-service.js
+++ app/services/job-families-service.js
@@ -1,1 +1,15 @@
-//placeholder
+
+const axios = require('axios');
+
+exports.getJobFamilies = async () => {
+    let results;
+    await axios.get('http://localhost:8080/job-family/engineering')
+        .then(response => {
+            results = response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            //res.render('error-page', { error_code: error.response.data} )
+        });
+    return results;
+}
