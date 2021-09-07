8bc0dc0fb5a97080a728ff79ee987a85db9282c0
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-09-07 10:03:26 +0100

    add unit tests

--- app/unit-test/matrix-roles-test.test.js
+++ app/unit-test/matrix-roles-test.test.js
@@ -10,11 +10,8 @@ const axios = jest.mock("axios", () => {
     return mockAxios;
 })
 
-const { getMatrixRoles } = require("../services/matrix-roles-service");
-
 const matrixOfRolesRoutes = require('../routes/matrix-roles-route');
-const getJobRolesService = require("../services/matrix-roles-service");
-const { getMatrixRoles } = require("../services/matrix-roles-service");
+const getMatrixRoles = require("../services/matrix-roles-service");
 
 app.use(express.urlencoded({ extended: false }));
 app.use("/viewmatrixofroles", matrixOfRolesRoutes);
@@ -24,57 +21,43 @@ describe("Test the route", () => {
     request(app)
       .get("/")
       .expect("view-matrix-roles")
-      .expect(getJobRolesService.getMatrixRoles());
+      .expect(getMatrixRoles.getMatrix());
   });
 })
 
-describe("Test the job roles service endpoint", () => {
-    test("The results should return job role list", async () => {
+describe("Test the matrix service endpoint", () => {
+    test("The results should return the matrix", async () => {
         let expected = {
             data: [
-                    { 
-                        'job_family': 'Engineering Strategy and Planning',
-                        'management_levels': [
-                            {'level': 'Leadership Community', 'jobs':  [{'title':'Chief Technology Officer (CTO)', 'title':'Technology Lead'}]},
-                            {'level': 'Principal', 'jobs':  [{}]},     
-                            {'level': 'Manager', 'jobs':  [{}]},     
-                            {'level': 'Consultant', 'jobs':  [{}]},     
-                            {'level': 'Senior Associate', 'jobs':  [{}]},     
-                            {'level': 'Associate', 'jobs':  [{}]},     
-                            {'level': 'Trainee', 'jobs':  [{}]},     
-                            {'level': 'Apprentice', 'jobs':  [{}]}     
+                    {
+                        'bandName':'Leadership Community',
+                        'jobFamilies':[
+                            {
+                                'jobFamilyName':'Engineering Strategy and Planing', 
+                                'jobTitles': [
+                                    'Chief Technology Officer (CTO)',
+                                    'Technology Lead'
+                                ]
+                            }
                         ]
                     },
-                    {
-                        'job_family': 'Engineering',
-                        'management_levels': [
-                            {'level': 'Leadership Community', 'jobs':  [{}]},
-                            {'level': 'Principal', 'jobs':  [{}]},     
-                            {'level': 'Manager', 'jobs':  [{}]},     
-                            {'level': 'Consultant', 'jobs':  [{'title':'Lead Software Engineer'}]},
-                            {'level': 'Senior Associate', 'jobs':  [{'title':'Senior Software Engineer', 'title':'Senior Front-End Engineer'}]},
-                            {'level': 'Associate', 'jobs':  [{'title':'Software Engineer', 'title':'Front-End Engineer'}]},
-                            {'level': 'Trainee', 'jobs':  [{'title':'Software Engineer'}]},
-                            {'level': 'Apprentice', 'jobs':  [{'title':'Software Engineer'}]},
-                        ]
-                    }
                 ]
         };
         mockAxios.get.mockImplementation(() => {
             return Promise.resolve(expected);
         });
-        let results = await getMatrixRoles();
+        let results = await getMatrixRoles.getMatrix();
         expect(results).toBe(expected.data);
-        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8080/'); //Needs to be modified
+        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8080/job-matrix/engineering');
     });
 
     test("The results should return an undefined list", async () => {
         mockAxios.get.mockImplementation(() => {
             return Promise.reject();
         });
-        let results = await getMatrixRoles();
+        let results = await getMatrixRoles.getMatrix();
         expect(results).toBe(undefined);
-        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8080/'); //Needs to be modified
+        expect(mockAxios.get).toHaveBeenCalledWith('http://localhost:8080/job-matrix/engineering');
     })
 
 })
