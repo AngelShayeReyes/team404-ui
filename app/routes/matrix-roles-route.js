7a3cba3ccec8d9512affb2cb90cdc1358945604e
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-09-07 09:54:23 +0100

    edit the njk to work with the json returned by the api endpoint

--- app/routes/matrix-roles-route.js
+++ app/routes/matrix-roles-route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const matrixRolesService = require('../services/matrix-roles-service');
 
 router.get("/", async (req, res) => {
-    res.render('view-matrix-roles', {bands: await matrixRolesService.getBands(), jobFamiliesOnly: await matrixRolesService.getJobFamilies()});
+    res.render('view-matrix-roles', {matrix: await matrixRolesService.getMatrix()});
 });
 
 module.exports = router
--- app/services/matrix-roles-service.js
+++ app/services/matrix-roles-service.js
@@ -1,20 +1,8 @@
 const axios = require('axios');
 
-exports.getBands = async () => {
+exports.getMatrix = async () => {
     let results;
-    await axios.get('http://localhost:8080/API_ENDPOINT')
-        .then(response => {
-            results = response.data;
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    return results;
-}
-
-exports.getJobFamilies = async () => {
-    let results;
-    await axios.get('http://localhost:8080/job-family')
+    await axios.get('http://localhost:8080/job-matrix/engineering')
         .then(response => {
             results = response.data;
         })
