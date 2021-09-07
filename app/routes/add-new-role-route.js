66ccfe79a0c18b9080cede9a6619495755332379
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-09-07 16:18:13 +0100

    change capability/jobFamily to text input

--- app/routes/add-new-role-route.js
+++ app/routes/add-new-role-route.js
@@ -8,10 +8,31 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    res.locals.errormessage = "Not implemented yet!";
-    console.log(req.body);
-    let status = await addNewRoleService.addNewRole(req.body);
-    res.render('add-new-role', {bands: await addNewRoleService.getAllBands(), locations: await addNewRoleService.getAllLocations(), data: req.body, status: status});
+    locations = await addNewRoleService.getAllLocations();
+    
+    selectedLocations = [];
+    for (let i = 0; i < locations.length; i++ ){
+        if (eval('req.body.location' + (i+1))) {
+            selectedLocations.push(eval('req.body.location' + (i+1)))
+        }
+    }
+    
+    var newRole = {
+        title: req.body.title,
+        description: req.body.description,
+        contractType: req.body.contractType,
+        locations: selectedLocations,
+        capability: req.body.capability,
+        responsibilities: req.body.responsibilities,
+        band: req.body.band,
+        jobFamily: req.body.jobFamily,
+        sharepointLink: req.body.sharepointLink
+    }
+
+    let status = await addNewRoleService.addNewRole(newRole);
+    res.render('add-new-role', {bands: await addNewRoleService.getAllBands(), locations: locations, data: req.body, status: status});
+    
+    console.log(newRole)
 });
 
 module.exports = router
